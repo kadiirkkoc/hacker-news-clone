ab013e7d51ad334570005a10d1051b3624a534d6
Kadir Koç <kadiirkkoc@gmail.com>
2023-04-09 05:06:25 +0200

    part7 is done

--- components/Story.js
+++ components/Story.js
@@ -18,9 +18,9 @@ export default function Story(story) {
                     ${story.comments_count} comments
                     </a>
                     |
-                    <span class="favorite">
+                    <span class="favorite" data-story=' ${JSON.stringify(story)} '>
                         <img class="heart" src="https://icon.now.sh/heart/ccc">
-                        Add To Favorites
+                        ${story.isFavorite ? "Remove from favorites" : "Add favorites"}
                     </span>
                 </div>
             </div>
--- pages/stories.js
+++ pages/stories.js
@@ -1,12 +1,35 @@
 import Story from "../components/Story.js"
 import view from "../utils/view.js"
 import baseUrl from "../utils/baseUrl.js";
+import store from "../store.js";
+import checkFavorite from "../utils/checkFavorite.js";
+
 
 export default async function Stories(path){
+    const {favorites} = store.getState() // we take the favorites array from store.getData( )
     const stories = await getStories(path)
     const hasStories = stories.length > 0;
 
-    view.innerHTML =  `<div>${hasStories ? stories.map((story,i) => Story({...story,index: i+1})).join("") : "no stories"}</div>`
+    view.innerHTML =  `<div>
+    ${hasStories ? stories.map((story,i) => Story({...story,index: i+1, isFavorite : 
+        checkFavorite(favorites,story)})).join("") : "no stories"}</div>`
+
+    document.querySelectorAll(".favorite").forEach(favoriteButton =>{
+        favoriteButton.addEventListener("click", async function(){
+            const story = JSON.parse(this.dataset.story)
+
+            const isFavorited = checkFavorite(favorites,story)
+            
+            if(isFavorited){
+                store.dispatch({type : "REMOVE_FAVORITE" , payload : { favorite : story } })
+            }
+            else{
+                store.dispatch({type : "ADD_FAVORITE" , payload : { favorite : story } })
+            }
+            await Stories(path)
+
+        })
+    })
 }
 
 async function getStories(path){
@@ -22,3 +45,11 @@ async function getStories(path){
    const stories = await response.json()
    return stories
 }
+
+
+// https://node-hnapi.herokuapp.com
+
+// / (Top) -> /news
+// /new (New) -> /newest
+// /ask (Ask) -> /ask
+// /show (Show) -> /show 
